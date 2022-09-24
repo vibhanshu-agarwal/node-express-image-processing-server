929da78888b37c925a3d8c4bc0f812fc6c54ce01
vibhanshu-agarwal <105557418+vibhanshu-agarwal@users.noreply.github.com>
2022-09-24 15:59:46 +0530

    Completed Module

---
+++ api/app.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const app = require("express")();
+
+const pathToIndex = path.join(__dirname, "../client/index.html");
+
+app.use("/*", function (req, res) {
+    //Transfers the file at the given path.
+    res.sendFile(pathToIndex);
+});
+
+module.exports = app;
