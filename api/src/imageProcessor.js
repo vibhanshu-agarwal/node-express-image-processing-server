703f24ba86835331c97e2ad99db51fd58f82348e
vibhanshu-agarwal <105557418+vibhanshu-agarwal@users.noreply.github.com>
2022-09-25 02:37:07 +0530

    Completed Module

---
+++ api/src/imageProcessor.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const { Worker, isMainThread } = require("worker_threads");
+const { tryCatch } = require("ramda");
+const pathToResizeWorker = path.resolve(__dirname, "resizeWorker.js");
+const pathToMonoChromeWorker = path.resolve(__dirname, "monochromeWorker.js");
+
+//Create the upload path resolver function
+function uploadPathResolver(filename) {
+  return path.resolve(__dirname, "../uploads", filename);
+}
+
+module.exports = function imageProcessor(filename) {
+  const sourcePath = uploadPathResolver(filename);
+  const resizedDestination = uploadPathResolver("resized-" + filename);
+  const monochromeDestination = uploadPathResolver("monochrome-" + filename);
+  let resizeWorkerFinished = false;
+  let monoChromeWorkerFinished = false;
+  return new Promise((resolve, reject) => {
+    if (isMainThread) {
+      //Do something??
+      try {
+        const resizeWorker = new Worker(pathToResizeWorker, {
+          workerData: { source: sourcePath, destination: resizedDestination },
+        });
+        resizeWorker.on("message", (message) => {
+          resizeWorkerFinished = true;
+          if (monoChromeWorkerFinished) {
+            resolve("resizeWorker finished processing");
+          }
+        });
+        resizeWorker.on("error", (error) => {
+          reject(new Error(error.message));
+        });
+        resizeWorker.on("exit", (code) => {
+          if (code !== 0) {
+            reject(new Error(`Exited with status code ${code}`));
+          }
+        });
+
+        const monochromeWorker = new Worker(pathToMonoChromeWorker, {
+          workerData: {
+            source: sourcePath,
+            destination: monochromeDestination,
+          },
+        });
+        monochromeWorker.on("message", (message) => {
+          monoChromeWorkerFinished = true;
+          if (resizeWorkerFinished) {
+            resolve("monochromeWorker finished processing");
+          }
+        });
+        monochromeWorker.on("error", (error) => {
+          reject(new Error(error.message));
+        });
+        monochromeWorker.on("exit", (code) => {
+          if (code !== 0) {
+            reject(new Error(`Exited with status code ${code}`));
+          }
+        });
+      } catch (error) {
+        reject(error);
+      }
+    } else {
+      reject(new Error("not on main thread"));
+    }
+  });
+};
--- api/src/router.js
+++ api/src/router.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const path = require("path");
 //Resolve the path to the photo viewer
 const photoPath = path.resolve(__dirname, "../../client/photo-viewer.html");
+const imageProcessor = require("./imageProcessor");
 
 const storage = multer.diskStorage({
   destination: "api/uploads/",
@@ -18,12 +19,18 @@ const upload = multer({
 router.get("/photo-viewer", (request, response) => {
   response.sendFile(photoPath);
 });
-
-router.post("/upload", upload.single("photo"), (req, res) => {
+//Mark our callback function as async in our post() method on the /upload route
+router.post("/upload", upload.single("photo"),  async (req, res) => {
   if (req.fileValidationError) {
     res.status(400).json({ error: req.fileValidationError });
   } else {
-    res.status(201).json({ success: true });
+    try {
+        await imageProcessor(req.file.filename);
+        res.status(201).json({ success: true });
+    }
+    catch (error) {
+        console.log(error);
+    }
   }
 });
 
